9531fff889bea6f018c032a54abd114b0271e6a0
shanm19 <shanm19@gmail.com>
2016-09-08 12:58:22 -0600

    refactor firstRepeat.js

--- assignments/warmups/first-input/first-input.js
+++ assignments/warmups/first-input/first-input.js
@@ -2,14 +2,12 @@ function firstRepeat(str) {
 
     var letters = {};
 
+    // adds key to letters if it doesn't exist, otherwise it increments
     for (var i = 0; i < str.length; i++) {
-        if (!letters.hasOwnProperty(str[i])) {
-            letters[str[i]] = 1;
-        } else {
-            letters[str[i]]++;
-        }
+        (letters.hasOwnProperty(str[i])) ? letters[str[i]]++ : letters[str[i]] = 1;
     }
 
+    // finds first letter in letters that is less than 1
     for (var letter in letters) {
         if (letters[letter] === 1) {
             return letter;
